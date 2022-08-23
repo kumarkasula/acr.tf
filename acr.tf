112e2831c19f1e35e1d38127d5e259deaa462299
kumarkasula <106813293+kumarkasula@users.noreply.github.com>
2022-08-23 18:25:31 +0530

    Add files via upload

---
+++ acr.tf
@@ -0,0 +1,18 @@
+
+resource "azurerm_container_registry" "acr" {
+  name                = "containerRegistry1"
+  resource_group_name = "DEV-TF-RG"
+  location            = "eastus"
+  sku                 = "Standard"
+  admin_enabled       = false
+  georeplications {
+    location                = "East US"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+  georeplications {
+    location                = "westeurope"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+}
